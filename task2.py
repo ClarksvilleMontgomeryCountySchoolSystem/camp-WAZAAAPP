3b0ba95e82709902022be78609063ed2526aac7c
Eli Leutert <Elijah.Leutert594@students.cmcss.net>
2026-02-11 08:59:03 -0600

    y

--- task2.py
+++ task2.py
@@ -1,40 +1,37 @@
 from data import first, last, birth, days, p_first, p_last, phone, street, city, state, zip_code
 
 # For each variable convert to the appropriate data type.
-
-
 # Not all variables will need conversion.
 
-
 # first
-
+first = str(first)
 
 # last
-
+last = str(last)
 
 # birth
-
+birth = int(birth)
 
 # days
-
+days = int(days)
 
 # p_first
-
+p_first = str(p_first)
 
 # p_last
-
+p_last = str(p_last)
 
 # phone
-
+phone = str(phone)
 
 # street
-
+street = str(street)
 
 # city
-
+city = str(city)
 
 # state
-
+state = str(state)
 
 # zip_code
-
+zip_code = str(zip_code)
